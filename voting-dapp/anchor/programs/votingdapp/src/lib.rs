e90ad85ae3a7c98ee87d208e86a8254a902fdab0
pyronlaboratory <ronnie94official@gmail.com>
2025-04-19 16:52:20 +0100

    Updated initialize poll fn

--- voting-dapp/anchor/programs/votingdapp/src/lib.rs
+++ voting-dapp/anchor/programs/votingdapp/src/lib.rs
@@ -12,14 +12,24 @@ pub mod votingdapp {
         poll_description: String,
         poll_start: u64,
         poll_end: u64,
-        poll_candidate_amount: u64,
     ) -> Result<()> {
         let poll: &mut Account<'_, Poll> = &mut ctx.accounts.poll;
         poll.id = poll_id;
         poll.description = poll_description;
         poll.start = poll_start;
         poll.end = poll_end;
-        poll.candidate_amount = poll_candidate_amount;
+        poll.candidate_amount = 0;
+        Ok(())
+    }
+
+    pub fn initialize_candidate(
+        ctx: Context<InitializeCandidate>,
+        candidate_name: String,
+        _poll_id: u64,
+    ) -> Result<()> {
+        let candidate: &mut Account<'_, Candidate> = &mut ctx.accounts.candidate;
+        candidate.name = candidate_name;
+        candidate.votes = 0;
         Ok(())
     }
 }
@@ -28,7 +38,7 @@ pub mod votingdapp {
 #[instruction(poll_id: u64)]
 pub struct InitializePoll<'info> {
     #[account(mut)]
-    user: Signer<'info>,
+    pub user: Signer<'info>,
     #[account(
         init,
         payer = user,
@@ -36,8 +46,29 @@ pub struct InitializePoll<'info> {
         seeds = [poll_id.to_le_bytes().as_ref()],
         bump
     )]
-    poll: Account<'info, Poll>,
-    system_program: Program<'info, System>,
+    pub poll: Account<'info, Poll>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>,
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMATOR_SIZE + Candidate::INIT_SPACE,
+        seeds = [candidate_name.as_bytes(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub candidate: Account<'info, Candidate>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
@@ -49,4 +80,12 @@ pub struct Poll {
     pub start: u64,
     pub end: u64,
     pub candidate_amount: u64,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate {
+    #[max_len(100)]
+    pub name: String,
+    pub votes: u64,
 }
--- voting-dapp/anchor/tests/votingdapp.spec.ts
+++ voting-dapp/anchor/tests/votingdapp.spec.ts
@@ -1,42 +1,77 @@
 import { BN, Program } from "@coral-xyz/anchor";
 import { PublicKey } from "@solana/web3.js";
 import { BankrunProvider, startAnchor } from "anchor-bankrun";
+import { ProgramTestContext } from "solana-bankrun";
 import { Votingdapp } from "../target/types/votingdapp";
 
 const IDL = require("../target/idl/votingdapp.json");
 
 const votingAddress = new PublicKey(
   "coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF"
-); // smart contract address
+);
 
 describe("Voting dApp::Test__Suite", () => {
-  it("Initialize Poll", async () => {
-    const context = await startAnchor(
+  let context: ProgramTestContext;
+  let provider: BankrunProvider;
+  let votingProgram: Program<Votingdapp>;
+
+  beforeAll(async () => {
+    context = await startAnchor(
       "",
       [{ name: "votingdapp", programId: votingAddress }],
       []
     );
-    const provider = new BankrunProvider(context);
-    const votingProgram = new Program<Votingdapp>(IDL, provider);
+    provider = new BankrunProvider(context);
+    votingProgram = new Program<Votingdapp>(IDL, provider);
+  });
+
+  it("Initialize Poll", async () => {
     await votingProgram.methods
       .initializePoll(
         new BN(1),
         "UFC 324: Athlete Red v. Athlete Blue: Round 1/3",
         new BN(0),
-        new BN(1745061937),
-        new BN(2)
+        new BN(1745061937)
       )
       .rpc();
-
     const [pollAddress] = PublicKey.findProgramAddressSync(
       [new BN(1).toArrayLike(Buffer, "le", 8)],
       votingAddress
     );
     const poll = await votingProgram.account.poll.fetch(pollAddress);
+
     expect(poll.id.toNumber()).toBe(1);
     expect(poll.description).toEqual(
       "UFC 324: Athlete Red v. Athlete Blue: Round 1/3"
     );
     expect(poll.start.toNumber()).toBeLessThan(poll.end.toNumber());
   });
+
+  it("Initialize Candidate", async () => {
+    await votingProgram.methods
+      .initializeCandidate("Nate Diaz", new BN(1))
+      .rpc();
+    await votingProgram.methods
+      .initializeCandidate("Dustin Poirer", new BN(1))
+      .rpc();
+    const [redCandidateAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("Nate Diaz"), new BN(1).toArrayLike(Buffer, "le", 8)],
+      votingAddress
+    );
+    const redCandidate = await votingProgram.account.candidate.fetch(
+      redCandidateAddress
+    );
+    const [blueCandidateAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("Dustin Poirer"), new BN(1).toArrayLike(Buffer, "le", 8)],
+      votingAddress
+    );
+    const blueCandidate = await votingProgram.account.candidate.fetch(
+      blueCandidateAddress
+    );
+
+    expect(redCandidate.votes.toNumber()).toBe(0);
+    expect(blueCandidate.votes.toNumber()).toBe(0);
+  });
+
+  it("Vote", async () => {});
 });
